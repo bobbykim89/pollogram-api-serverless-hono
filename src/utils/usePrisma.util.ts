0f7ef817eb246359871705fb430320cd8e56e2ac
Sihun Kim <skim585@asu.edu>
2025-05-16 12:56:33 -0700

    feat(src/utils/): remove lines regarding websocket constructor
    remove lines related to websocket constructor
    
    develop

--- src/utils/usePrisma.util.ts
+++ src/utils/usePrisma.util.ts
@@ -2,15 +2,15 @@ import { PrismaClient } from '../db/prisma'
 import { UseConfig } from './useConfig.util'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaNeon } from '@prisma/adapter-neon'
-import { neonConfig } from '@neondatabase/serverless'
-import * as ws from 'ws'
+// import { neonConfig } from '@neondatabase/serverless'
+// import * as ws from 'ws'
 
 export class UsePrisma extends PrismaClient {
   constructor(config: UseConfig) {
     super({
       adapter: new PrismaNeon({ connectionString: config.databaseUrl }),
     })
-    neonConfig.webSocketConstructor = ws
+    // neonConfig.webSocketConstructor = ws
   }
   public warmup = async () => {
     try {
