1e22b8f289c0252be66e6282459058364772df1c
Sihun Kim <skim585@asu.edu>
2025-05-16 15:28:35 -0700

    feat(lib/,src/): code cleanup
    update github workflow for prod-stage,remove comments and unused dependencies.
    
    develop

--- src/utils/usePrisma.util.ts
+++ src/utils/usePrisma.util.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from '../db/prisma'
 import { UseConfig } from './useConfig.util'
 import { PrismaPg } from '@prisma/adapter-pg'
-import { neonConfig } from '@neondatabase/serverless'
-import * as ws from 'ws'
 
 export class UsePrisma extends PrismaClient {
   constructor(config: UseConfig) {
